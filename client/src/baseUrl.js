a6970ff4ca3e7ef41edcdd305306aa3100333ff9
vishwasGarade <139865344+vishwasGarade@users.noreply.github.com>
2023-07-24 17:08:14 +0530

    Update baseUrl.js

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://hybrid-app-mern-test-4.azurewebsites.net";
+const baseUrl = "https://hybrid-app-mern-test-4.azurewebsites.net";
 
 // http://hybrid-app-mern-test-4.azurewebsites.net
 export default baseUrl;
